15911ca0fc8e94bc27eed3c580e270c50c784932
RITZ-31 <ritzmohapatra31@gmail.com>
2026-08-27 20:53:00 +0530

    Configure backend for Vercel serverless

--- server/server.js
+++ server/server.js
@@ -1,18 +1,8 @@
-// require("dotenv").config();
-// const app = require("./app");
-// const connectDb = require("./config/db.js");
-// const PORT = process.env.PORT || 5000;
 require("dotenv").config();
-
-console.log("MONGODB_URL exists:", !!process.env.MONGODB_URL);
 
 const app = require("./app");
 const connectDb = require("./config/db.js");
-const PORT = process.env.PORT || 5000;
-//connect to mongodb
+
 connectDb();
 
-
-app.listen(PORT ,() =>{
-  console.log(`Server is running on port ${PORT}`);
-})
+module.exports = app;
