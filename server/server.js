81b5c0beb4949d8d7f7a8d96138ac5b48515759a
RITZ-31 <ritzmohapatra31@gmail.com>
2026-08-27 20:43:19 +0530

    Check production database environment

--- server/server.js
+++ server/server.js
@@ -1,8 +1,14 @@
+// require("dotenv").config();
+// const app = require("./app");
+// const connectDb = require("./config/db.js");
+// const PORT = process.env.PORT || 5000;
 require("dotenv").config();
+
+console.log("MONGODB_URL exists:", !!process.env.MONGODB_URL);
+
 const app = require("./app");
 const connectDb = require("./config/db.js");
 const PORT = process.env.PORT || 5000;
-
 //connect to mongodb
 connectDb();
 
